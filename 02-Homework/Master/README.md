697bcb826e63e8f2aa1f8863a385a3f90093aea6
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-05-09 16:36:30 -0400

    Homework9

---
+++ 02-Homework/Master/README.md
@@ -0,0 +1,56 @@
+
+# ParkMe
+[![GitHub license](https://img.shields.io/badge/license-undefined-blue.svg)](https://github.com/Youstina/parkme)
+
+## Description
+
+Find Park Me
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+undefined
+```
+
+## Usage
+
+yes
+
+## License
+
+This project is licensed under the undefined license.
+  
+## Contributing
+
+yes
+
+## Tests
+
+To run tests, run the following command:
+
+```
+undefined
+```
+
+## Questions
+
+<img src="https://avatars3.githubusercontent.com/u/7957506?v=4" alt="avatar" style="border-radius: 16px" width="30" />
+
+If you have any questions about the repo, open an issue or contact [Youstina](https://api.github.com/users/youstina) directly at null.
+
---
+++ 02-Homework/Master/index.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  }
+];
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log("Searching...");
+
+    api
+      .getUser(inquirerResponses.github)
+      .then(({ data }) => {
+        writeToFile("README.md", generateMarkdown({ ...inquirerResponses, ...data }));
+      })
+  })
+}
+
+init();
